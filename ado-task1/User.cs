91e27cbd523245804a2db7d0a4a387cb952ea870
lenovo <lenovo@DESKTOP-O95QVK8>
2024-07-28 13:05:17 +0400

    Add project files.

---
+++ ado-task1/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ado_task1
+{
+    class User
+    {
+        public string _username { get; set; }
+        public string _passsword { get; set; }
+        public string _name { get; set; }
+        public string _surname { get; set; }
+        public int _age { get; set; }
+    }
+}
